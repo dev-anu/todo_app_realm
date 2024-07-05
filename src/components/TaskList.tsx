75161ffa8e4a5f22d4cf4c6e8c73fb6d2fa1523d
dev-anu <anu.infy850@gmail.com>
2024-07-05 15:52:33 +0530

    added TestFiled and local tasklist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { View, Text,FlatList } from 'react-native'
+import { View, Text,FlatList, TextInput,StyleSheet, Button } from 'react-native'
 import {useState} from 'react'
 import TaskListItem from './TaskListItem'
 
@@ -8,6 +8,13 @@ const TaskList = () => {
         {description:'First Task'}
     ]);
 
+    const [newTask,setNewTask] = useState('');
+
+    const createTask=()=>{
+        setTask([...tasks,{description:newTask}])
+        setNewTask('');
+    }
+
   return (
     <View>
       <Text>Todo</Text>
@@ -15,8 +22,26 @@ const TaskList = () => {
      contentContainerStyle={{gap:5}}
      renderItem={({item}:any)=> <TaskListItem description={item?.description}/>}
      />  
+     <TextInput
+     value={newTask}
+     onChangeText={setNewTask}
+        placeholder='New Task'
+        placeholderTextColor="grey"
+        style={styles.textField}
+     />
+     <Button title='Add TasK' onPress={createTask}/>
     </View>
   )
 }
 
 export default TaskList
+
+const styles=StyleSheet.create({
+    textField:{
+        color:"white",
+        padding:10,
+        backgroundColor:'#1D2125',
+        borderRadius:5,
+        marginTop:5
+    }
+})
